a65e682d34f6bd8f42b45cef947b730184d5c216
Jephthah Akunnor Annor <jephthahannor123@gmail.com>
2026-08-02 23:16:33 +0000

    Implement multiplication table functions
    Added functions to print multiplication tables for a given number and up to N.

--- assignment_06_multiplication_table.cpp
+++ assignment_06_multiplication_table.cpp
@@ -55,5 +55,42 @@
 // =============================================================================
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+void printTable(int num) {
+    cout << "Multiplication Table for " << num << ":" << endl;
+    for (int i = 1; i <= 12; i++) {
+        cout << num << " x " << setw(2) << i << " = " << num * i << endl;
+    }
+}
+
+void printTablesUpToN(int n) {
+    if (n <= 0) {
+        cout << "Error: N must be a positive integer." << endl;
+        return;
+    }
+
+    for (int num = 1; num <= n; num++) {
+        printTable(num);
+        if (num != n) {
+            cout << "---------------------------" << endl;
+        }
+    }
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    printTable(num);
+
+    cout << endl;
+
+    int n;
+    cout << "Enter a number to generate tables from 1 to N: ";
+    cin >> n;
+    printTablesUpToN(n);
+
+    return 0;
+}
